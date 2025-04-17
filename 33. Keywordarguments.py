b1918fc80929fe79d4dc6b82563d63d7a36deb9a
jatinrajput26 <138561926+jatinrajput26@users.noreply.github.com>
2025-04-17 23:55:06 +0530

    Add files via upload

---
+++ 33. Keywordarguments.py
@@ -0,0 +1,28 @@
+# Keyword arguments = An argument preceded by an identifier 
+                    #   helps with readability
+                    #  order of arguments doen"t matter 
+                    # 1. positional 2. default 3.KEYWORD 4.arbitrary 
+
+
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title}{first}{last}")
+
+# hello (greeting = "Hello" , title = "Mr." , first = "Spongebob" , last = "Squarepants" )  
+# hello (greeting = "Hello" , title = "Mr.", last = "Squarepants" ,  first = "Spongebob") 
+# with keyword argument position doesmt matter it will give acc to upper value 
+# hello ("Hello" , "Mr.", last = "Squarepants" ,  first = "Spongebob") 
+
+# for x in range (1,11):
+#     print(x , end = " ") #here end is keyword arguments 
+
+
+#--SEP KEYWORD ARGUMEMTS 
+# print("1", "2", "3", "4", "5", sep = "- ")
+
+#-------------EXAMPLE:
+
+# def get_phone (country , area , first , last ):
+#     return f"{country}-{area}-{first}-{last}"
+
+# print(get_phone(country = "usa", area = "vsx", first = "js", last = "sjk"))
+    
